cbef5a8e2c3022397a1f83b334f4e4ae19a7aa72
Kieran <kieran.conlon@firecresthorizon.uk>
2024-11-05 15:20:20 +0000

    Update to add public visibility

--- Sources/PerformanceTimer/PerformanceTimer.swift
+++ Sources/PerformanceTimer/PerformanceTimer.swift
@@ -7,44 +7,44 @@ import Foundation
 /// A stopwatch timer using DispatchTime for nano-second time resolution
 /// The reported values for laps, stop and elapsed times are a `Double` with
 ///  the time units being set in `timerReportingUnits` when the timer is created.
-struct PerformanceTimer {
+public struct PerformanceTimer {
   private(set) var times: [DispatchTime] = []
   private var isRunning: Bool = false
-  let reportingUnits: PerformanceTimerUnits
+  public let reportingUnits: PerformanceTimerUnits
   
   /// Time between the start and stop (or last lap) times
-  var total: Double {
+  public var total: Double {
     guard let first = times.first, let last = times.last else { return 0.0 }
     return reportingUnits.convert(from: first.distance(to: last))
   }
   
   /// Array of the lap times
-  var laps: [Double] {
+  public var laps: [Double] {
     zip(times, times.dropFirst()).map { previous, current in
       reportingUnits.convert(from: previous.distance(to: current))
     }
   }
   
   /// Elapsed time from the start time to .now() or the stop time if the timer has been stopped
-  var elapsed: Double {
+  public var elapsed: Double {
     guard let startTime = times.first else { return 0.0 }
     let endTime = isRunning ? DispatchTime.now() : times.last!
     
     return reportingUnits.convert(from: startTime.distance(to: endTime))
   }
   
-  init(reportingUnits: PerformanceTimerUnits = .nanoseconds) {
+  public init(reportingUnits: PerformanceTimerUnits = .nanoseconds) {
     self.reportingUnits = reportingUnits
   }
   
   /// Clear the previous times, record the start time and set the timer as running
-  mutating func start() {
+  public mutating func start() {
     reset()
     _ = lap()
   }
   
   /// Record the lap time, then calculate the interval from the previous start/lap and return the value
-  mutating func lap() -> Double {
+  public mutating func lap() -> Double {
     isRunning = true
     let lapTime = DispatchTime.now()
     guard let previousTime = times.last else {
@@ -56,23 +56,23 @@ struct PerformanceTimer {
   }
   
   /// Record the stop time, set the timer to stopped, then calculate the interval from the previous start/lap and return the value
-  mutating func stop() -> Double {
+  public mutating func stop() -> Double {
     let lapTime = lap()
     isRunning = false
     return lapTime
   }
   
   /// Stop the timer and clear stored lap times
-  mutating func reset() {
+  public mutating func reset() {
     isRunning = false
     times.removeAll()
   }
 }
 
-enum PerformanceTimerUnits {
+public enum PerformanceTimerUnits {
   case seconds, milliseconds, microseconds, nanoseconds
   
-  var label: String {
+  public var label: String {
     switch self {
     case .seconds:      return "s"
     case .milliseconds: return "ms"
@@ -82,7 +82,7 @@ enum PerformanceTimerUnits {
   }
   
   /// Convert a `DispatchTimeInterval` to a time interval units of this enum
-  func convert(from interval: DispatchTimeInterval) -> Double {
+  public func convert(from interval: DispatchTimeInterval) -> Double {
     switch interval {
     case .seconds(let val), .milliseconds(let val), .microseconds(let val), .nanoseconds(let val):
       return Double(val) * interval.intervalMultiplier / nanoSecondsMultiplier
